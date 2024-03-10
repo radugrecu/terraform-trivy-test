3043a79de8d9fa23850f243cc54a003b812b602e
Radu Grecu <radugrecu@noreply.users.github.com>
2024-03-10 08:25:48 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+
+data "aws_vpc" "test1" {
+    default = true
+}
+
+resource "aws_security_group" "test1" {
+    name        = "test1"
+    description = "Allow inbound traffic"
+    vpc_id      = data.aws_vpc.test1.id
+}
+
+resource "aws_security_group_rule" "test1" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.test1.id}"
+}
+
