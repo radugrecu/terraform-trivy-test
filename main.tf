c89889d9d032306e451bea4875bd830b6afcaf7b
Radu Grecu <radugrecu@noreply.users.github.com>
2024-03-10 08:27:55 +0200

    some changes

--- main.tf
+++ main.tf
@@ -9,7 +9,29 @@ resource "aws_security_group" "test1" {
     vpc_id      = data.aws_vpc.test1.id
 }
 
+resource "aws_security_group_rule" "test0" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.test1.id}"
+}
+
+
+
 resource "aws_security_group_rule" "test1" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.test1.id}"
+}
+
+
+
+resource "aws_security_group_rule" "test2" {
     type        = "ingress"
     from_port   = 0
     to_port     = 65535
