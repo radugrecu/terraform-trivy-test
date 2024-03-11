8c158fd055d1c9533a59ecd2f6731af5b258a9af
Radu Grecu <radugrecu@users.noreply.github.com>
2024-03-11 09:26:05 +0200

    another insecure sgr

--- another.tf
+++ another.tf
@@ -6,3 +6,12 @@ resource "aws_security_group_rule" "another_test0" {
     cidr_blocks = ["0.0.0.0/0"]
     security_group_id = "${aws_security_group.test1.id}"
 }
+
+resource "aws_security_group_rule" "another_test1" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.test1.id}"
+}
