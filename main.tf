c86584aaeed15a59c3f2c4afc8d3b39c01a15624
Radu Grecu <radugrecu@noreply.users.github.com>
2024-03-11 07:23:39 +0200

    add another TF fail

--- main.tf
+++ main.tf
@@ -40,3 +40,13 @@ resource "aws_security_group_rule" "test2" {
     security_group_id = "${aws_security_group.test1.id}"
 }
 
+
+
+resource "aws_security_group_rule" "test3" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.test1.id}"
+}
