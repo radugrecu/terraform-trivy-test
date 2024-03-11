055f7776236cdc3c3580e915d3610ba60a7e4f77
Radu Grecu <radugrecu@users.noreply.github.com>
2024-03-11 11:11:35 +0200

    add egress sgr

--- another.tf
+++ another.tf
@@ -17,3 +17,13 @@ resource "aws_security_group_rule" "another_test1" {
 }
 
 // a comment
+
+
+resource "aws_security_group_rule" "egress_test1" {
+    type        = "egress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.test1.id}"
+}
