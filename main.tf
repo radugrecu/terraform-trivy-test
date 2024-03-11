ec588fb117ade32366cd3ad4e8290c5e7e227daa
Radu Grecu <radugrecu@noreply.users.github.com>
2024-03-11 08:47:41 +0200

    add another insecure sgr

--- main.tf
+++ main.tf
@@ -40,9 +40,17 @@ resource "aws_security_group_rule" "test2" {
     security_group_id = "${aws_security_group.test1.id}"
 }
 
-
-
 resource "aws_security_group_rule" "test3" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.test1.id}"
+}
+
+
+resource "aws_security_group_rule" "test4" {
     type        = "ingress"
     from_port   = 0
     to_port     = 65535
