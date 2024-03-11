7d02d492f4faae6830dc5059b5b9e0db449dcc96
Radu Grecu <radugrecu@noreply.users.github.com>
2024-03-11 08:55:28 +0200

    more errors

---
+++ another.tf
@@ -0,0 +1,8 @@
+resource "aws_security_group_rule" "another_test0" {
+    type        = "ingress"
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    security_group_id = "${aws_security_group.test1.id}"
+}
